0a3ae05a68b93fa5fa7962609dc97011a0b74811
classiccitytv <aorekyeh@ccity.tv>
2025-10-19 15:35:52 -0400

    Update register.ts

--- packages/common/src/modules/register.ts
+++ packages/common/src/modules/register.ts
@@ -79,19 +79,32 @@ container.bind(EpgService).to(ViewNexaEpgService).whenNamed(EPG_TYPE.viewNexa);
 container.bind(INTEGRATION_TYPE).toDynamicValue(getIntegrationType);
 container.bind(API_ACCESS_BRIDGE_URL).toDynamicValue(getApiAccessBridgeUrl);
 
-
+// Cleeng integration
+//container.bind(DETERMINE_INTEGRATION_TYPE).toConstantValue(isCleengIntegrationType);
+//container.bind(CleengService).toSelf();
+//container.bind(AccountService).to(CleengAccountService).whenNamed(INTEGRATION.CLEENG);
+//container.bind(CheckoutService).to(CleengCheckoutService).whenNamed(INTEGRATION.CLEENG);
+//container.bind(SubscriptionService).to(CleengSubscriptionService).whenNamed(INTEGRATION.CLEENG);
 
 // JWP integration
-container.bind(DETERMINE_INTEGRATION_TYPE).toConstantValue(isJwpIntegrationType);
-container.bind(JWPAPIService).toSelf();
-container.bind(JWPEntitlementService).toSelf();
-container.bind(AccountService).to(JWPAccountService).whenNamed(INTEGRATION.JWP);
-container.bind(CheckoutService).to(JWPCheckoutService).whenNamed(INTEGRATION.JWP);
-container.bind(SubscriptionService).to(JWPSubscriptionService).whenNamed(INTEGRATION.JWP);
+//container.bind(DETERMINE_INTEGRATION_TYPE).toConstantValue(isJwpIntegrationType);
+//container.bind(JWPAPIService).toSelf();
+//container.bind(JWPEntitlementService).toSelf();
+//container.bind(AccountService).to(JWPAccountService).whenNamed(INTEGRATION.JWP);
+//container.bind(CheckoutService).to(JWPCheckoutService).whenNamed(INTEGRATION.JWP);
+//container.bind(SubscriptionService).to(JWPSubscriptionService).whenNamed(INTEGRATION.JWP);
 
-// Cleeng integration
-container.bind(DETERMINE_INTEGRATION_TYPE).toConstantValue(isCleengIntegrationType);
-container.bind(CleengService).toSelf();
-container.bind(AccountService).to(CleengAccountService).whenNamed(INTEGRATION.CLEENG);
-container.bind(CheckoutService).to(CleengCheckoutService).whenNamed(INTEGRATION.CLEENG);
-container.bind(SubscriptionService).to(CleengSubscriptionService).whenNamed(INTEGRATION.CLEENG);
+
+const useCleeng = process.env.REACT_APP_INTEGRATION_TYPE === 'cleeng';
+
+if (useCleeng) {
+  container.bind(DETERMINE_INTEGRATION_TYPE).toConstantValue(isCleengIntegrationType);
+  container.bind(AccountService).to(CleengAccountService).whenNamed(INTEGRATION.CLEENG);
+  container.bind(CheckoutService).to(CleengCheckoutService).whenNamed(INTEGRATION.CLEENG);
+  container.bind(SubscriptionService).to(CleengSubscriptionService).whenNamed(INTEGRATION.CLEENG);
+} else {
+  container.bind(DETERMINE_INTEGRATION_TYPE).toConstantValue(isJwpIntegrationType);
+  container.bind(AccountService).to(JWPAccountService).whenNamed(INTEGRATION.JWP);
+  container.bind(CheckoutService).to(JWPCheckoutService).whenNamed(INTEGRATION.JWP);
+  container.bind(SubscriptionService).to(JWPSubscriptionService).whenNamed(INTEGRATION.JWP);
+}
